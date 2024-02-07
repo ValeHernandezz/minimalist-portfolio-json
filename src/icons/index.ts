028e195951b64ad35e5921c6f7878f88fb54aa70
ValeHernandezz <valehernandezmodino@gmail.com>
2024-02-07 02:14:19 -0300

    Add index for icons

---
+++ src/icons/index.ts
@@ -0,0 +1,41 @@
+import AstroIcon from '@/icons/skills/AstroIcon.astro';
+import CSS from '@/icons/skills/Css3.astro';
+import Git from '@/icons/skills/Git.astro';
+import GitHub from '@/icons/GitHub.astro';
+import GitLab from '@/icons/skills/GitLab.astro';
+import Hibernate from '@/icons/skills/Hibernate.astro';
+import HTML from '@/icons/skills/Html5.astro';
+import Infraestructura from '@/icons/skills/Infraestructura.astro';
+import Java from '@/icons/skills/Java.astro';
+import JavaScript from '@/icons/skills/JavaScript.astro';
+import JSP from '@/icons/skills/Jsp.astro';
+import LinkedIn from '@/icons//LinkedIn.astro';
+import Mail from '@/icons//Mail.astro';
+import Phone from '@/icons//Phone.astro';
+import React from '@/icons/skills/React.astro';
+import SQL from '@/icons/skills/Sql.astro';
+import Tailwind from '@/icons/skills/TailwindCSS.astro';
+import Testing from '@/icons/skills/Testing.astro';
+import WorldMap from '@/icons//WorldMap.astro';
+
+export {
+  AstroIcon,
+  CSS,
+  Git,
+  GitHub,
+  GitLab,
+  Hibernate,
+  HTML,
+  Infraestructura,
+  Java,
+  JavaScript,
+  JSP,
+  LinkedIn,
+  Mail,
+  Phone,
+  React,
+  SQL,
+  Tailwind,
+  Testing,
+  WorldMap,
+};
